29d59492a9df0cee70c90e150fa6334ec0783d28
Shih-Kai Lin <kaikai581@hotmail.com>
2019-07-29 22:43:45 -0500

    My first working page!

---
+++ src/components/models/CSRNet.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>{{ message }}</p>
+        <p><input v-model="message"></p>
+    </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component({})
+export default class CSRNet extends Vue{
+    data() {
+        return {message: 'Hello Vue!'};
+    }
+}
+</script>
